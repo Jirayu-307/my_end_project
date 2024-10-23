4adbe72a53b3d95ad49674c347764f5073f90c9b
Jirayut Suansamran <jiiirayu2546@gmail.com>
2024-10-24 03:03:43 +0700

    Create superuser (Admin)

--- app_rooms/admin.py
+++ app_rooms/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from app_rooms.models import Room
 
 # Register your models here.
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = ['title']
+    
+admin.site.register(Room, RoomAdmin)
