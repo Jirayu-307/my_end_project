f1e2b63ac4e60f41216965bcf5c8a7d1f613da73
Jirayut Suansamran <jiiirayu2546@gmail.com>
2024-10-24 02:54:36 +0700

    Add little more filters

--- app_rooms/views.py
+++ app_rooms/views.py
@@ -1,37 +1,20 @@
 from datetime import datetime
 from django.http.response import HttpResponse
 from django.shortcuts import render
-
-all_rooms = [
-    {
-        'id': 1, 'title': 'ห้องพักหมายเลข 1', 'price': 1699, 'is_multiple_bed': True,
-        'check_in_date': datetime(2024, 1, 1), 'check_out_date': datetime(2024, 1, 10)
-    },
-    {
-        'id': 2, 'title': 'ห้องพักหมายเลข 2', 'price': 1499, 'is_multiple_bed': False,
-        'check_in_date': datetime(2024, 1, 1), 'check_out_date': datetime(2024, 1, 10)
-    },
-    {
-        'id': 3, 'title': 'ห้องพักหมายเลข 3', 'price': 1699, 'is_multiple_bed': True,
-        'check_in_date': datetime(2024, 1, 1), 'check_out_date': datetime(2024, 1, 10)
-    },
-    {
-        'id': 4, 'title': 'ห้องพักหมายเลข 4', 'price': 1499, 'is_multiple_bed': False,
-        'check_in_date': datetime(2024, 1, 1), 'check_out_date': datetime(2024, 1, 10)
-    },
-]
+from .models import Room
 
 # Create your views here.
 
 def rooms(request):
+    all_rooms = Room.objects.all()
     context = { 'rooms': all_rooms }
     return render(request, 'app_rooms/rooms.html', context)
 
 def room(request, room_id):
     one_room = None
     try:
-        one_room = [r for r in all_rooms if r['id'] == room_id][0]
-    except IndexError:
+        one_room = Room.objects.get(id=room_id)
+    except:
         print('ไม่พบห้องพักนี้')
     context = { 'room': one_room }
     return render(request, 'app_rooms/room.html', context)
