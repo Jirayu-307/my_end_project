b45f56733057aad8cdcce482bc6a1f55792149b8
Jirayut Suansamran <jiiirayu2546@gmail.com>
2024-10-24 02:20:29 +0700

    Use tags on templates

--- app_rooms/views.py
+++ app_rooms/views.py
@@ -1,10 +1,24 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+all_rooms = [
+    {'id': 1, 'title': 'ห้องพักหมายเลข 1', 'price': 699, 'is_multiple_bed': True},
+    {'id': 2, 'title': 'ห้องพักหมายเลข 2', 'price': 499, 'is_multiple_bed': False},
+    {'id': 3, 'title': 'ห้องพักหมายเลข 3', 'price': 699, 'is_multiple_bed': True},
+    {'id': 4, 'title': 'ห้องพักหมายเลข 4', 'price': 499, 'is_multiple_bed': False},
+]
+
 # Create your views here.
 
 def rooms(request):
-    return render(request, 'app_rooms/rooms.html')
+    context = { 'rooms': all_rooms }
+    return render(request, 'app_rooms/rooms.html', context)
 
 def room(request, room_id):
-    return render(request, 'app_rooms/room.html', context={ 'room_id': room_id })
+    one_room = None
+    try:
+        one_room = [r for r in all_rooms if r['id'] == room_id][0]
+    except IndexError:
+        print('ไม่พบห้องพักนี้')
+    context = { 'room': one_room }
+    return render(request, 'app_rooms/room.html', context)
